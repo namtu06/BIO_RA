cec3cc4027c119a4c55231ea4ac6a327c80533a9
namtu06 <namtuhoanghai06@gmail.com>
2026-03-29 21:07:36 +0300

    project restructure

--- src/meta_processing.py
+++ src/meta_processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-general_df2 = pd.read_csv("./data/metadata.csv")
+general_df2 = pd.read_csv("../data/processed/metadata.csv")
 general_df2 = general_df2.set_index("id")
 
 labels = general_df2[general_df2["WHO grade"].isin(["III", "III with necrosis", "III without necrosis","IV", "IV with necrosis", "IV without necrosis"])]
@@ -33,7 +33,7 @@ print(f"Prolif: {len(III_prim_Prolif.index)}")
 
 print("\n")
 # Recurrent
-print("RECURRENT IV")
+print("RECURRENT III")
 III_rec_PN = (III_recurrent[III_recurrent["Subtype"] == "PN"])
 III_rec_Mes = (III_recurrent[III_recurrent["Subtype"] == "Mes"])
 III_rec_Prolif = (III_recurrent[III_recurrent["Subtype"] == "Prolif"])
